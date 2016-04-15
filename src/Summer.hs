5f95e7c332678681e0ab1efb4b056c87e93ded63
mschaper <michael.schaper@uibk.ac.at>
2016-04-15 20:45:26 +0200

    render nicer table to html

--- src/Summer.hs
+++ src/Summer.hs
@@ -6,6 +6,7 @@ import qualified Data.Map.Strict                 as M
 import           Data.Monoid
 import qualified Data.Set                        as S
 import qualified System.Directory                as Dir
+import           System.FilePath.Posix           ((<.>), (</>))
 import           System.Process
 -- import           System.Environment (getArgs)
 
@@ -15,7 +16,7 @@ import           Text.Hamlet
 import           Runner
 import           Util
 
-import Debug.Trace
+import           Debug.Trace
 
 
 synopsis :: String
@@ -35,7 +36,7 @@ gather :: [FilePath] -> IO DB
 gather ts = normalise <$> traverse gatherOne ts
   where
     gatherOne t = do
-      fs <- lines <$> readCreateProcess (shell $ "find " <> t <> " -type f -name '*.result'") mempty
+      fs <- lines <$> readCreateProcess (shell $ "find " <> t <> " -type f -name '*.result' -o -name '*.err'") mempty
       rs <- (M.fromList . fmap (\r -> (rProblem r, Just r))) <$> traverse deserialise fs
       return $ Column{cHead = t, cRows = rs}
     normalise cs = fillMissing  `fmap` cs
@@ -56,30 +57,61 @@ mkTable db              = M.assocs `fmap` foldr merge ([],M.empty) db
 -- writeResults :: [Result] -> IO ()
 -- writeResults = writeTable
 
+toEntry :: Result -> (String, Outcome String, Double)
+toEntry Result{rTool=t,rProblem=p,rTime=z,rOutcome=out} = (fp,out,z)
+  where fp = tName t </> p <.> tExtension t
+
+-- FIXME: MS: differentitate between error and proof when giving the link; I suppose shamlet can do Either
 writeTable :: FilePath -> DB -> IO()
 writeTable fp db = do
   let
     fmap5 = fmap . fmap . fmap . fmap . fmap
-  let (ts,ps) = fmap5 (show . rOutcome) $ mkTable db
+  let (ts,ps) = fmap5 toEntry $ mkTable db
   writeFile fp $ renderHtml
     [shamlet|
 $doctype 5
 <html>
+  <link rel="stylesheet" type="text/css" href="http://cl-informatik.uibk.ac.at/software/tct/includes/experiments.css">
+  <script type="text/javascript" src="http://cl-informatik.uibk.ac.at/software/tct/includes/table.js">
   <head>
     <title> experiment
   <body>
-    <table>
-      <tr>
-        <td>
-        $forall t <- ts
-          <td>#{t}
-      $forall (p,rs) <- ps
+    <h1> Details
+    <table class="table-autofilter table-autosort table-filtered-rowcount:cnt">
+      <thead>
         <tr>
-          $forall mr <- rs
-            <td>#{p}
-              $maybe r <- mr
-                <td>#{r}
+          <th>
+          $forall t <- ts
+            <th colspan="3">
+              <div class="toolname">#{t}
+        <tr>
+          <th class="table-sortable:default table-sortable">
+            <div class="lhd">Problem (<span id="cnt">all</span> selected)
+          $forall t <- ts
+            <th> *
+            <th class="table-filterable">answer
+            <th class="table-sortable:numeric table-sortable">time
+      <tbody>
+        $forall (p,rs) <- ps
+          <tr>
+            <td>
+              <div class="lhd">#{p}
+            $forall mr <- rs
+              $maybe (l,o,z) <- mr
+                <td>
+                  <a href="#{l}">*
+                <td>
+                  $case o
+                    $of Success out
+                      <a href="#{l}.proof">#{out}
+                    $of Failure err
+                      <a href="#{l}.err">ERROR
+                    $of Timeout
+                      TIMEOUT
+                <td>#{z}
               $nothing
+                <td>
                 <td> missing
+                <td> -
     |]
 
